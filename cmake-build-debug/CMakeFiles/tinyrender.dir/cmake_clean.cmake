0f847d7653926c6a2bba571eea6af745fecdbd58
nocis <nocis@tju.edu.cn>
2020-09-09 18:59:16 -0700

    cmake build

---
+++ cmake-build-debug/CMakeFiles/tinyrender.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyrender.dir/src/examples/fluid1/main.cpp.o"
+  "bin/tinyrender"
+  "bin/tinyrender.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyrender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/src/engine/CMakeFiles/engine_header_gen_py.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "../../../include/engine.h"
+  "CMakeFiles/engine_header_gen_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/engine_header_gen_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
